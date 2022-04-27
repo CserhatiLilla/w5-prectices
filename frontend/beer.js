72354144ae57ea3daa6443ff759bb36cf7c1c882
CserhatiLilla <lillacserhati2@gmail.com>
2022-04-27 09:44:11 +0200

    added beer.js and data.js also changed index.html

---
+++ frontend/beer.js
@@ -0,0 +1,32 @@
+console.log(beers);
+
+const beersTitleComponent = function (inner,sub){
+    return `
+    <section id="${sub}"> ${inner} </section>
+`
+}
+
+const beerCardComponent = function (title,sub,text){
+
+    return`
+    <div class="card">
+        <beer detail="${title}">${title}</beer>
+        <beer detail="${sub}">${sub}</beer>
+        <beer detail="${text}">${text}</beer>
+    </div>
+    `
+}
+
+const loadEvent = function(){
+    const rootElement = document.getElementById("root");
+    
+    for (let index = 1; index <= sub.text; index++) {
+        
+        let sectionInner = ""
+        sectionInner += dayCardComponent(title, sub.sub, index)
+    }
+    console.log(sectionInner);
+
+    rootElement.insertAdjacentHTML("beforeend", beersTitleComponent(sectionInner, sub.text))
+
+}
